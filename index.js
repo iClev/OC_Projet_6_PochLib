f31947e3b007b4f2e86872717965a12d7d41d652
clevyd <c.mbizibikouta@lpcr.fr>
2021-06-05 01:46:43 +0200

    authored-by: iClev <clevyd.mb@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let nouveauLivre = document.querySelector(".h2");
 let title = document.querySelector(".title");
-let resultDivMessage = document.createElement("div");
+// let resultDivMessage = document.createElement("div");
 const content = document.getElementById("content");
 const container = document.getElementById("myBooks");
 
-let pochList = [];
+// let pochList = [];
 
 // Funtion Button add book
 function addBookButton() {
@@ -44,25 +44,71 @@ function addSearchForm() {
 
 function searchBook() {
   const url = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
-  const titleInput = document.getElementById("title");
-  const authorInput = document.getElementById("author");
 
   fetch(url)
     .then((res) => res.json())
     .then((results) => {
+      const cardContainer = document.createElement('div');
+      cardContainer.className = 'card-container';
+
       const search = results.items;
       search.map((book) => {
         console.log("book => ", book);
+        const card = document.createElement('div');
+        card.className = 'card';
 
+        const idBookCard = document.createElement('h4');
+        idBookCard.innerText = "Id : " + book.id;
+        idBookCard.className = 'card-id';
 
+        const titleBookCard = document.createElement('h4');
+        titleBookCard.innerText = "Titre : " + book.volumeInfo.title;
+        titleBookCard.className = 'card-title';
 
+        const authorBookCard = document.createElement('p');
+        authorBookCard.innerText = "Auteur : " + book.volumeInfo.authors;
+        authorBookCard.className = 'card-author';
+        if (book.volumeInfo.authors > 1) {
+          book.volumeInfo.authors = book.volumeInfo.authors.slice(0, 2);
+        }
 
+        const descriptionBookCard = document.createElement('p');
+        descriptionBookCard.innerText = "Description : " + book.volumeInfo.description;
+        descriptionBookCard.className = 'card-description';
+        if (descriptionBookCard === '' || descriptionBookCard === 'undefined') {
+          descriptionBookCard.innerText = "Information manquante";
+        } else if (descriptionBookCard.innerText.length > 200) {
+          descriptionBookCard.innerText = descriptionBookCard.innerText.substring(0, 200) + '...';
+        }
 
+        const bookMarkCard = document.createElement('span');
+        bookMarkCard.className = 'card-bookMark';
 
+        const bookmark = document.createElement('i');
+        bookmark.className = 'card-fav';
+        bookMarkCard.innerHTML = '<i class="fas fa-bookmark"></i>';
+        bookmark.setAttribute('class', 'fas fa-bookmark')
+        bookmark.setAttribute('style', 'font-size: 6em; color: #1c938c')
 
+        const imgCard = document.createElement('img');
+        imgCard.className = 'card-img';
 
+        if (book.volumeInfo.imageLinks === null || book.volumeInfo.imageLinks === undefined) {
+          imgCard.src = 'images/unavailable.png';
+        } else {
+          imgCard.src = book.volumeInfo.imageLinks.thumbnail;
+        }
 
+        cardContainer.appendChild(card);
+        card.appendChild(bookMarkCard);
+        bookMarkCard.appendChild(bookMark);
+        card.appendChild(titleBookCard);
+        card.appendChild(idBookCard);
+        card.appendChild(authorBookCard);
+        card.appendChild(descriptionBookCard);
+        card.appendChild(imgCard);
       });
+      document.getElementById('content').appendChild(cardContainer);
     });
 }
 
@@ -81,22 +127,6 @@ function searchResults() {
   searchButton.after(searchResults);
 }
 
-// Create PochList
-// function createPochList() {
-//   let pochListDiv = document.createElement("div");
-//   pochListDiv.classList.add("pochList");
-//   content.after(pochListDiv);
-//   console.log(sessionStorage.getItem("savedBooks"));
-
-//   //Display the saved books
-//   if (sessionStorage.getItem("savedBooks")) {
-//     pochList = JSON.parse(sessionStorage.getItem("savedBooks"));
-//     showPochlist();
-//   }
-//   console.log(sessionStorage.getItem("savedBooks"));
-// }
-// createPochList();
-
 //Create alert
 function alertMessage() {
   let alert = document.createElement("div");
@@ -104,9 +134,21 @@ function alertMessage() {
 }
 alertMessage();
 
-// function showPochlist() {
-//   pochListDiv.innerHTML = '';
+function addToFavorites(book) {
+  bookMark.addEventListener('click', (e) => {
+    e.preventDefault();
+    let books = [];
+    if ((sessionStorage.getItem('book') !== null) && (sessionStorage.getItem('book') !== undefined)) {
+      books = JSON.parse(sessionStorage.getItem('book'));
+    }
 
-// }
+    if (books.some(b => b.id === book.id)) {
+      alert('Ajout impossible, le livre est déjà présent dans vos favoris.');
+    } else {
+      books.push(book);
+      sessionStorage.setItem('book', JSON.stringify(books));
+    }
+  })
+}
 
 
